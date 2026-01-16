e9e835e2d77baff25b358f2cc9ef7a9d49dc2499
Yang1ee <100892928+Yang1ee@users.noreply.github.com>
2026-01-16 13:43:13 +0800

    Delete JavaCalculator directory

--- JavaCalculator/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new CalculatorGUI());
-    }
-}
